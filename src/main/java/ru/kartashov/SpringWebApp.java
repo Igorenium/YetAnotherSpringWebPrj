5c8be82fbf7800460ec6a6a167fc9fa098bf330e
Igor Kartashov <lplp456810@gmail.com>
2023-02-10 21:24:38 +0300

    Change injection of dependency

--- src/main/java/ru/kartashov/SpringWebApp.java
+++ src/main/java/ru/kartashov/SpringWebApp.java
@@ -1,15 +1,14 @@
 package ru.kartashov;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import ru.kartashov.model.Music;
 
 public class SpringWebApp {
 
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
 
-        Music music = context.getBean("musicBean", Music.class);
-        new MusicPlayer(music).playMusic();
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        musicPlayer.playMusic();
 
         context.close();
     }
