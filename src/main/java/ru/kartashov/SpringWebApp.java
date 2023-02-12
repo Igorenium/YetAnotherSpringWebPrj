2463cc3bee45e08dfec2eb874a73e068d835486f
Igor Kartashov <lplp456810@gmail.com>
2023-02-12 15:16:30 +0300

    Change code to test prototype scope

--- src/main/java/ru/kartashov/SpringWebApp.java
+++ src/main/java/ru/kartashov/SpringWebApp.java
@@ -7,10 +7,20 @@ public class SpringWebApp {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
 
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
+        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
+
+        System.out.println(musicPlayer1 == musicPlayer2);
+        System.out.println(musicPlayer1);
+        System.out.println(musicPlayer2);
+
+        System.out.println(musicPlayer1.getVolume());
+        System.out.println(musicPlayer2.getVolume());
+
+        musicPlayer1.setVolume(10);
+
+        System.out.println(musicPlayer1.getVolume());
+        System.out.println(musicPlayer2.getVolume());
 
         context.close();
     }
