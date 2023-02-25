c51a55c7ce34fb9dfbc04e85e49379297943acb3
Igor Kartashov <lplp456810@gmail.com>
2023-02-25 18:12:29 +0300

    Pass attributes through the model

--- src/main/java/ru/kartashov/controller/MainController.java
+++ src/main/java/ru/kartashov/controller/MainController.java
@@ -1,6 +1,7 @@
 package ru.kartashov.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -9,15 +10,37 @@ public class MainController {
 
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", required = false) String name,
-                        @RequestParam(value = "surname", required = false) String surname) {
+                        @RequestParam(value = "surname", required = false) String surname,
+                        Model model) {
 
-        System.out.println("Hello, " + name + " " + surname);
+        model.addAttribute("message", "Hello, " + name + " " + surname);
 
         return "main/hello";
     }
     
-    @GetMapping("/goodbye")
-    public String goobdye() {
-        return "main/goodbye";
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam("oper") String operator,
+                             @RequestParam("a") int a,
+                             @RequestParam("b") int b,
+                             Model model) {
+        String result = "Result of " + operator + ": ";
+        switch (operator) {
+            case "addition":
+                result += a + b;
+                break;
+            case "subtraction":
+                result += a -b;
+                break;
+            case "multiplication":
+                result += a * b;
+                break;
+            case "division":
+                result += a / b;
+                break;
+            default:
+                return "main/error";
+        }
+        model.addAttribute("result", result);
+        return "main/calculator";
     }
 }
